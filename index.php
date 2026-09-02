4713dd8b98fcc5ce3f87c7c9f884caf8044e4dd1
rahulmails1649-tech <rahulmails1649@gmail.com>
2026-09-02 10:26:38 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,13 @@
 <!-- Mirrored from gurlypets.online/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 01 Sep 2026 12:49:57 GMT -->
 <!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 <head>
+	<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "yc3x1o8xye");
+</script>
 	<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-DNWMR4G2CV"></script>
 <script>
